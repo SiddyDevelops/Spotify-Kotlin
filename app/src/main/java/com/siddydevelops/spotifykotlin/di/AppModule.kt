41caa72d373762f2eea9e02671684c6a3631c08e
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-18 15:16:15 +0530

    Implement di

---
+++ app/src/main/java/com/siddydevelops/spotifykotlin/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.siddydevelops.spotifykotlin.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.siddydevelops.spotifykotlin.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions().placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_image)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+
+}

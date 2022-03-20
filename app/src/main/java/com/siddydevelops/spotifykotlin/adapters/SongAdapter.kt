ddfe3c8645a155e87e54001ad9feeaf7edcc31f2
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-20 22:24:47 +0530

    Added Adapters-init

---
+++ app/src/main/java/com/siddydevelops/spotifykotlin/adapters/SongAdapter.kt
@@ -0,0 +1,11 @@
+package com.siddydevelops.spotifykotlin.adapters
+
+import androidx.appcompat.view.menu.ActionMenuItemView
+import com.bumptech.glide.RequestManager
+import javax.inject.Inject
+
+class SongAdapter @Inject constructor(
+    private val glide: RequestManager
+) {
+    class SongViewHolder(itemView: ActionMenuItemView)
+}

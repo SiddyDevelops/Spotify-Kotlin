5198b17698b7a784c673550d0bf3aa1f02eb0b0e
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-27 22:09:37 +0530

    Added song callback

--- app/src/main/java/com/siddydevelops/spotifykotlin/ui/fragments/SongFragment.kt
+++ app/src/main/java/com/siddydevelops/spotifykotlin/ui/fragments/SongFragment.kt
@@ -1,19 +1,25 @@
 package com.siddydevelops.spotifykotlin.ui.fragments
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
 import com.siddydevelops.spotifykotlin.R
 import com.siddydevelops.spotifykotlin.data.entities.Song
+import com.siddydevelops.spotifykotlin.exoplayer.isPlaying
 import com.siddydevelops.spotifykotlin.exoplayer.toSong
 import com.siddydevelops.spotifykotlin.other.Status
 import com.siddydevelops.spotifykotlin.ui.viewModels.MainViewModel
 import com.siddydevelops.spotifykotlin.ui.viewModels.SongViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_song.*
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -26,11 +32,47 @@ class SongFragment : Fragment(R.layout.fragment_song) {
     private val songViewModel: SongViewModel by viewModels()
 
     private var curPlayingSong: Song? = null
+    private var  playbackState: PlaybackStateCompat? = null
+    private var shouldUpdateSeekbar = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         subscribeToObservers()
+
+        ivPlayPauseDetail.setOnClickListener {
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it,true)
+            }
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser){
+                    setCurPlayerTimeToTextView(progress.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                shouldUpdateSeekbar = false
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                seekBar?.let {
+                    mainViewModel.seekTo(it.progress.toLong())
+                    shouldUpdateSeekbar = true
+                }
+            }
+        })
+
+        ivSkipPrevious.setOnClickListener {
+            mainViewModel.skipToPreviousSong()
+        }
+
+        ivSkip.setOnClickListener {
+            mainViewModel.skipToNextSong()
+        }
+
     }
 
     private fun updateTitleAndSongImage(song:Song) {
@@ -60,6 +102,32 @@ class SongFragment : Fragment(R.layout.fragment_song) {
             updateTitleAndSongImage(curPlayingSong!!)
         }
 
+        mainViewModel.playbackState.observe(viewLifecycleOwner) {
+            playbackState = it
+            ivPlayPauseDetail.setImageResource(
+                if(playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+            seekBar.progress = it?.position?.toInt() ?: 0
+        }
+
+        songViewModel.curPlayerPostiton.observe(viewLifecycleOwner) {
+            if(shouldUpdateSeekbar) {
+                seekBar.progress = it.toInt()
+                setCurPlayerTimeToTextView(it)
+            }
+        }
+
+        songViewModel.curSongDuration.observe(viewLifecycleOwner){
+            seekBar.max = it.toInt()
+            val dateFormat = SimpleDateFormat("mm:ss",Locale.getDefault())
+            tvSongDuration.text = dateFormat.format(it)
+        }
+
+    }
+
+    private fun setCurPlayerTimeToTextView(ms: Long) {
+        val dateFormat = SimpleDateFormat("mm:ss",Locale.getDefault())
+        tvCurTime.text = dateFormat.format(ms)
     }
 
 }

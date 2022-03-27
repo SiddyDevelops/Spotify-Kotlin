6ff4d1e054daa0466acd3039b406c2939494db33
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-27 16:31:03 +0530

    Added global navhost

---
+++ app/src/main/java/com/siddydevelops/spotifykotlin/ui/fragments/SongFragment.kt
@@ -0,0 +1,7 @@
+package com.siddydevelops.spotifykotlin.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.siddydevelops.spotifykotlin.R
+
+class SongFragment : Fragment(R.layout.fragment_song) {
+}
